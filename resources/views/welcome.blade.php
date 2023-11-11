5e430281b673077d9a773e4c1fb1b0b9b7341a84
shafiq-2000 <shafiq53471@gmail.com>
2023-11-11 11:04:28 +0600

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hellow Developer</title>
+</head>
+<body>
+
+<style>
+body {
+  background-color: lightblue;
+}
+
+h1{
+  margin-top:200px;
+  text-align:center;
+}
+
+</style>
+
+
+   <h1>Hellow Laravel Developer</h1> 
+
+
+</body>
+</html>
